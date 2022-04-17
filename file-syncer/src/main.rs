8a82d99e1260f6bb077ad941f3e614787bebe52f
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-04-17 10:53:39 -0400

    Revert "a"
    This reverts commit 3e0dacdaa1586838b659d894def5d622ba84a01e.

--- file-syncer/src/main.rs
+++ file-syncer/src/main.rs
@@ -1,24 +1,5 @@
-use std::io;
-
-pub struct fileandhash { 
-
-}
-
 fn main() {
-    println!("Rust music syncer!");
-    println!("is this the computer sending the files?");
-    let mut issender bool = true;
-    if issender = true {
-        let mut hashes vec!
-        // if we are the sender, get a vec of all the 
-        // hashes of the files in the specified dir
-        for elem in iter {
-
-            
-        }
-    } else {
-
-    }
+    println!("Hello, world!");
 }
 
 fn hashfileadd() {
