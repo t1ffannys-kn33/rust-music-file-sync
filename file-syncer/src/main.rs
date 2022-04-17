526d5dc24b74e139adfcf44c91bf3ed61419a917
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-04-17 11:20:16 -0400

    a

--- file-syncer/src/main.rs
+++ file-syncer/src/main.rs
@@ -1,17 +1,20 @@
 use std::io;
 
-pub struct fileandhash { 
-
+pub struct pathandhash { 
+    hash: String,
+    path:String
 }
 
 fn main() {
     println!("Rust music syncer!");
     println!("is this the computer sending the files?");
-    let mut issender bool = true;
-    if issender = true {
-        let mut hashes vec!
+    let mut issender = true;
+    if issender == true {
+        let mut hashes = Vec::new();
         // if we are the sender, get a vec of all the 
         // hashes of the files in the specified dir
+
+
         for elem in iter {
 
             
