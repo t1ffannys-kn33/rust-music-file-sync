0b72878b6050f1b6fb0524ebab525e8fe661c795
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-04-18 09:54:43 -0400

    b

--- file-syncer/src/main.rs
+++ file-syncer/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
+use std::{env, fs, io};
 
-pub struct pathandhash { 
+pub struct Pathandhash { 
     hash: String,
     path:String
 }
@@ -10,15 +10,13 @@ fn main() {
     println!("is this the computer sending the files?");
     let mut issender = true;
     if issender == true {
-        let mut hashes = Vec::new();
+        let mut hashes: Vec<Pathandhash> = vec![];
+        let currentdir = env::current_dir();
         // if we are the sender, get a vec of all the 
         // hashes of the files in the specified dir
+        println!("current dir is {:?}", currentdir);
 
 
-        for elem in iter {
-
-            
-        }
     } else {
 
     }
---
+++ file-syncer/target/debug/deps/file_syncer-de9ceb72bd0c916a.d
@@ -0,0 +1,5 @@
+/Users/tiffany/Documents/GitHub/ipatool/rust-file-sync/file-syncer/target/debug/deps/file_syncer-de9ceb72bd0c916a.rmeta: src/main.rs
+
+/Users/tiffany/Documents/GitHub/ipatool/rust-file-sync/file-syncer/target/debug/deps/file_syncer-de9ceb72bd0c916a.d: src/main.rs
+
+src/main.rs:
