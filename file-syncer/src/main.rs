91f5463aac1fd3c47f40e8bd48b8e2d49bf7faab
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-04-18 11:47:46 -0400

    a

--- file-syncer/src/main.rs
+++ file-syncer/src/main.rs
@@ -1,7 +1,8 @@
 use std::{env, fs, io};
+use glob::glob;
 
 pub struct Pathandhash { 
-    hash: String,
+    hash:String,
     path:String
 }
 
@@ -10,18 +11,21 @@ fn main() {
     println!("is this the computer sending the files?");
     let mut issender = true;
     if issender == true {
-        let mut hashes: Vec<Pathandhash> = vec![];
+        let mut hashes:Vec<Pathandhash> = Vec::new();
         let currentdir = env::current_dir();
         // if we are the sender, get a vec of all the 
         // hashes of the files in the specified dir
         println!("current dir is {:?}", currentdir);
-
-
+        //cool, we know where we are located, so we can start walking over all the sub
+        //directorys, and hash each file
+        for entry in fs::read_dir(currentdir)? {
+            println!()
+        }
     } else {
 
     }
 }
 
-fn hashfileadd() {
-  
-}
+fn 
+
+fn hashfileadd(){} 
