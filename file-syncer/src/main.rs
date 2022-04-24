1aed0a9246f919a77cde8c8a68d6f49dfdd92d94
cotton-candy-flag-enjoyer <trololol12446@gmail.com>
2022-04-24 01:51:43 -0600

    clean up

--- file-syncer/src/main.rs
+++ file-syncer/src/main.rs
@@ -2,18 +2,12 @@ use std::{env, fs, io};
 use glob::glob;
 use std::path::Path;
 
-
+#[derive(Debug)]
 pub struct Pathandhash { 
     hash:String,
     path:String
 }
 
-fn Hashandaddtovec(pathtofile:String) {
-    hashes.push(Pathandhash {
-
-        path:pathtofile,
-    })
-}
 
 fn main() {
 
@@ -35,35 +29,15 @@ fn main() {
     if issender == true { // we are the syncer
         for entry in glob("/Users/tiffany/Music/Soulseek Downloads/**").expect("Failed to read glob pattern") {
             match entry {
-                Ok(path) => hashes.push(new Pathandhash)(path.display().to_string()),
+                Ok(path) => hashes.push(Pathandhash {hash:path.display().to_string(), path:path.display().to_string(),}),
                 Err(e) => println!("{:?}", e),
             }
         }
+        println!("files assembled, ready to start pairing.")
+
     } else { // we are the syncee
         println!("you picked that this computer is not the sender");
     }
 
 }
-
-
-
-
-
-/*   broken code       
-let mut hashes:Vec<Pathandhash> = Vec::new();
-
-
-        let mut wheretoread = String::new;
-        let mut stdin = io::stdin(); // We get `Stdin` here.
-
-        stdin().read_line(&mut wheretoread)?;
-        println!({}, wheretoread);
-        // if we are the sender, get a vec of all the 
-        // hashes of the files in the specified dir
-        println!("current dir is {:?}", currentdir);
-        //cool, we know where we are located, so we can start walking over all the sub
-        //directorys, and hash each file
-        for entry in fs::read_dir(currentdir)? {
-            println!()
-            */
 
